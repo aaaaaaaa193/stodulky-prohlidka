ac6096cac894be7fc3d6b80f3575f25772963cdf
aaaaaaaa193 <pepik3277@gmail.com>
2025-06-08 19:02:02 +0200

    upload

---
+++ data.js
@@ -0,0 +1,827 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vstup",
+      "name": "Vstup",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.29010931719829003,
+        "pitch": 0.028946989735818107,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20629048281514173,
+          "pitch": 0.12673654675547041,
+          "rotation": 0,
+          "target": "12-les"
+        },
+        {
+          "yaw": -0.37931167258938814,
+          "pitch": 0.10404339818535036,
+          "rotation": 0,
+          "target": "1-jdelna-chodba"
+        },
+        {
+          "yaw": -1.961110133271637,
+          "pitch": 0.1601392283060541,
+          "rotation": 0,
+          "target": "7-chodba-klub"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-jdelna-chodba",
+      "name": "Jídelna-Chodba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.9172300812044947,
+        "pitch": 0.028973267904165212,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6476754907007898,
+          "pitch": 0.15597976127700264,
+          "rotation": 0,
+          "target": "15-prales"
+        },
+        {
+          "yaw": 1.4542450555353081,
+          "pitch": 0.21271820029518373,
+          "rotation": 0,
+          "target": "2-jdelna"
+        },
+        {
+          "yaw": -2.689212940763957,
+          "pitch": 0.15113656506560957,
+          "rotation": 0,
+          "target": "10-korl"
+        },
+        {
+          "yaw": 2.9146612432202703,
+          "pitch": 0.22844615672546986,
+          "rotation": 0,
+          "target": "5-chodba-piano-"
+        },
+        {
+          "yaw": -0.1622232840208877,
+          "pitch": 0.13008413561376386,
+          "rotation": 0,
+          "target": "0-vstup"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-jdelna",
+      "name": "Jídelna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -3.0727558914831548,
+        "pitch": 0.13476023524350467,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08232958973096238,
+          "pitch": 0.4208408392153071,
+          "rotation": 0,
+          "target": "1-jdelna-chodba"
+        },
+        {
+          "yaw": 2.9183540211778265,
+          "pitch": 0.2678802247159098,
+          "rotation": 0,
+          "target": "3-vdejna"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-vdejna",
+      "name": "Výdejna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.3891718073761368,
+        "pitch": 0.14857453934826914,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.42721755138541617,
+          "pitch": 0.2987352578319289,
+          "rotation": 0,
+          "target": "2-jdelna"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-chodba-k-prvodcovn",
+      "name": "Chodba k průvodcovně",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8393152072718522,
+          "pitch": 0.03485109668856978,
+          "rotation": 0,
+          "target": "8-jezero"
+        },
+        {
+          "yaw": -0.20615790232615794,
+          "pitch": 0.024524289720886827,
+          "rotation": 0,
+          "target": "14-metropole"
+        },
+        {
+          "yaw": 2.8850939163623712,
+          "pitch": 0.11109816672765227,
+          "rotation": 0,
+          "target": "6-chodba-u-kuchyky"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-chodba-piano-",
+      "name": "Chodba piano ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 3.1159298128533557,
+        "pitch": 0.09899116754566073,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9838697335857347,
+          "pitch": 0.14617232952407022,
+          "rotation": 0,
+          "target": "6-chodba-u-kuchyky"
+        },
+        {
+          "yaw": -1.0225190051344626,
+          "pitch": 0.18182439559750563,
+          "rotation": 0,
+          "target": "10-korl"
+        },
+        {
+          "yaw": -0.26152143639806624,
+          "pitch": 0.1380710737643298,
+          "rotation": 0,
+          "target": "1-jdelna-chodba"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-chodba-u-kuchyky",
+      "name": "Chodba u kuchyňky",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -3.0485862505484747,
+        "pitch": 0.12408246826992553,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.04696570981047,
+          "pitch": 0.20037154042685046,
+          "rotation": 0,
+          "target": "11-kuchyka"
+        },
+        {
+          "yaw": -2.367720210682002,
+          "pitch": 0.14881425346433375,
+          "rotation": 0,
+          "target": "17-sklenk"
+        },
+        {
+          "yaw": -1.4777280149942165,
+          "pitch": 0.20196156723377534,
+          "rotation": 0,
+          "target": "13-louka"
+        },
+        {
+          "yaw": -0.060025905887153286,
+          "pitch": 0.19069175958287232,
+          "rotation": 0,
+          "target": "5-chodba-piano-"
+        },
+        {
+          "yaw": 1.583629119619248,
+          "pitch": 0.16311482553683732,
+          "rotation": 0,
+          "target": "4-chodba-k-prvodcovn"
+        },
+        {
+          "yaw": 1.8413524001049604,
+          "pitch": 0.14793294976643523,
+          "rotation": 0,
+          "target": "19-zahrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-chodba-klub",
+      "name": "Chodba klub",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.0028206704067433,
+        "pitch": 0.07869088945289704,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11630479588251674,
+          "pitch": 0.17296350047440967,
+          "rotation": 0,
+          "target": "0-vstup"
+        },
+        {
+          "yaw": 2.8804698103446285,
+          "pitch": 0.007090984220429419,
+          "rotation": 0,
+          "target": "9-klub"
+        },
+        {
+          "yaw": 3.1011586702434,
+          "pitch": 0.022525644685035928,
+          "rotation": 0,
+          "target": "16-drna"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-jezero",
+      "name": "Jezero",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.24704373324410867,
+        "pitch": 0.1402233726763651,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2886579185500597,
+          "pitch": 0.20913000442687313,
+          "rotation": 0,
+          "target": "4-chodba-k-prvodcovn"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-klub",
+      "name": "Klub",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.314849765118433,
+          "pitch": 0.20422877076710577,
+          "rotation": 0,
+          "target": "7-chodba-klub"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-korl",
+      "name": "Korál",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.7307188560560238,
+        "pitch": 0.19418429064536902,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3678707579371086,
+          "pitch": 0.187487768617828,
+          "rotation": 0,
+          "target": "1-jdelna-chodba"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-kuchyka",
+      "name": "Kuchyňka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.2371235332545538,
+        "pitch": 0.08402049357053265,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0724087489399476,
+          "pitch": 0.31633776566792626,
+          "rotation": 0,
+          "target": "6-chodba-u-kuchyky"
+        },
+        {
+          "yaw": 2.071392128928995,
+          "pitch": 0.11626397128309307,
+          "rotation": 0,
+          "target": "17-sklenk"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-les",
+      "name": "Les",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3911570686828627,
+          "pitch": 0.09230982842675317,
+          "rotation": 0,
+          "target": "0-vstup"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-louka",
+      "name": "Louka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.940993202872944,
+        "pitch": 0.09182588258969027,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0630876319305482,
+          "pitch": 0.16570443838900495,
+          "rotation": 0,
+          "target": "6-chodba-u-kuchyky"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-metropole",
+      "name": "Metropole",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.596124251971851,
+        "pitch": 0.192153345276612,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.070523792336977,
+          "pitch": 0.1321546636357116,
+          "rotation": 0,
+          "target": "4-chodba-k-prvodcovn"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-prales",
+      "name": "Prales",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4077758677946637,
+          "pitch": 0.163708864046626,
+          "rotation": 0,
+          "target": "1-jdelna-chodba"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-drna",
+      "name": "Řádírna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5316062476699415,
+          "pitch": 0.06355591178206232,
+          "rotation": 0,
+          "target": "7-chodba-klub"
+        },
+        {
+          "yaw": 2.8686184497413993,
+          "pitch": 0.07129958001236503,
+          "rotation": 0,
+          "target": "18-stna"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-sklenk",
+      "name": "Skleník",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7277089832648542,
+          "pitch": 0.15211878023498215,
+          "rotation": 0,
+          "target": "6-chodba-u-kuchyky"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "18-stna",
+      "name": "Stěna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8329083405365907,
+          "pitch": 0.047632875289163934,
+          "rotation": 0,
+          "target": "16-drna"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "19-zahrada",
+      "name": "Zahrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.4122199641479458,
+        "pitch": 0.1499582076269661,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.596115065500749,
+          "pitch": 0.1541047113776166,
+          "rotation": 0,
+          "target": "6-chodba-u-kuchyky"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
